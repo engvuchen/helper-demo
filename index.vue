b5303ad15b38959dd2369e70088713970cad605c
v_zqianchen(陈志强) <v_zqianchen@tencent.com>
2021-09-06 20:53:20 +0800

    U 完成demo配置

--- index.vue
+++ index.vue
@@ -1,1 +1,82 @@
-<template></template>
+<template>
+  <div>
+    <!--1 : element-test-demo-el-tooltip-->
+    <el-tooltip
+      content=""
+    >
+    <el-tooltip/>
+
+    <!--2 : element-test-demo-el-pagination-->
+    <el-pagination
+      :total=""
+      :current-page="1"
+      layout="prev, pager, next, jumper, ->, total"
+      :page-sizes="[10, 20, 30, 40, 50, 100]"
+    >
+    <el-pagination/>
+
+    <!--3 : element-test-demo-el-row-->
+    <el-row
+    >
+    <el-row/>
+
+    <!--4 : element-test-demo-el-col-->
+    <!--5 : element-test-demo-el-radio-->
+    <!--6 : element-test-demo-el-table-->
+    <!--7 : element-test-demo-el-menu-->
+    <!--8 : element-test-demo-el-container-->
+    <!--9 : element-test-demo-el-checkbox-->
+    <!--10: element-test-demo-el-tag-->
+    <!--11: element-test-demo-el-tabs-->
+    <!--12: element-test-demo-el-input-->
+    <el-input
+      value=""
+    >
+    <el-input/>
+
+    <!--13: element-test-demo-el-progress-->
+    <!--14: element-test-demo-el-breadcrumb-->
+    <!--15: element-test-demo-el-input-number-->
+    <!--16: element-test-demo-el-tree-->
+    <!--17: element-test-demo-el-select-->
+    <!--18: element-test-demo-el-dropdown-->
+    <!--19: element-test-demo-el-icon-->
+    <!--20: element-test-demo-el-cascader-->
+    <!--21: element-test-demo-el-badge-->
+    <!--22: element-test-demo-el-steps-->
+    <!--23: element-test-demo-el-step-->
+    <!--24: element-test-demo-el-button-->
+    <el-button
+      type="success"
+    >
+    <el-button/>
+
+    <!--25: element-test-demo-el-switch-->
+    <!--26: element-test-demo-el-avatar-->
+    <!--27: element-test-demo-el-link-->
+    <!--28: element-test-demo-el-slider-->
+    <!--29: element-test-demo-el-skeleton-->
+    <!--30: element-test-demo-el-dialog-->
+    <!--31: element-test-demo-el-empty-->
+    <!--32: element-test-demo-el-popover-->
+    <!--33: element-test-demo-el-alert-->
+    <!--34: element-test-demo-el-popconfirm-->
+    <!--35: element-test-demo-loading-->
+    <!--36: element-test-demo-upload-->
+    <!--37: element-test-demo-el-card-->
+    <!--38: element-test-demo-el-rate-->
+    <!--39: element-test-demo-el-carousel-->
+    <!--40: element-test-demo-main-->
+    <!--41: element-test-demo-el-color-picker-->
+    <!--42: element-test-demo-el-collapse-->
+    <!--43: element-test-demo-main-->
+    <!--44: element-test-demo-el-transfer-->
+    <!--45: element-test-demo-el-timeline-->
+    <!--46: element-test-demo-el-form-->
+    <!--47: element-test-demo-el-divider-->
+    <!--48: element-test-demo-el-calendar-->
+    <!--49: element-test-demo-el-image-->
+    <!--50: element-test-demo-el-backtop-->
+    <!--51: element-test-demo-el-drawer-->
+  </div>
+</template>
